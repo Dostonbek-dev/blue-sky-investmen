78479aca1822bab952be285fab500de192a9ba05
Dostonbek Hamidov <dostonbekkhamidov@gmail.com>
2021-02-14 22:42:18 +0500

    Started creating website

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Scroll to top
+/*// Scroll to top
 const scrolltp = document.querySelector("#scrolltp");
 scrolltp.addEventListener("click", function () {
     window.scrollTo({
@@ -34,4 +34,47 @@ themeToggle.addEventListener("change", function () {
     } else {
         localStorage.removeItem("dark");
     }
-});
+});*/
+//Scroll to top
+const scrolltp=document.querySelector('#scrolltp');
+scrolltp.addEventListener('click',function (){
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth",
+    })
+})
+window.addEventListener('scroll',function(){
+    if (window.scrollY>=700){
+        scrolltp.style.opacity=1;
+    }else{
+        scrolltp.style.opacity=1;
+    }
+})
+/*Dark Mode*/
+const themeToggle=document.querySelector(".checkbox");
+const body=document.querySelector("body");
+const darkmode=localStorage.getItem("dark");
+
+if (darkmode) {
+    body.classList.add("dark");
+    themeToggle.checked=true;
+}
+themeToggle.addEventListener("change",function(){
+    body.classList.toggle("dark");
+
+    if (body.classList.contains('dark')){
+        localStorage.setItem("dark","active");
+    }else{
+        localStorage.removeItem("dark");
+    }
+})
+
+
+
+
+
+
+
+
+
